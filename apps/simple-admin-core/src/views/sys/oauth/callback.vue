a125ecc1c8ad746f25065530f381f1998e748d33
wuyang1581581 <wuyang158158@gmail.com>
2025-03-14 15:45:19 +0800

    callback login revert

--- apps/simple-admin-core/src/views/sys/oauth/callback.vue
+++ apps/simple-admin-core/src/views/sys/oauth/callback.vue
@@ -16,44 +16,28 @@ export default defineComponent({
   name: 'OauthCallbackPage',
   components: {},
   setup() {
-    let qs = null;
-    if (
-      router.currentRoute.value.redirectedFrom &&
-      router.currentRoute.value.redirectedFrom.query &&
-      router.currentRoute.value.redirectedFrom.query.code &&
-      router.currentRoute.value.redirectedFrom.query.state
-    ) {
-      qs = router.currentRoute.value.redirectedFrom.query;
-    }
-    if (
-      router.currentRoute.value.query.state &&
-      router.currentRoute.value.query.code
-    ) {
-      qs = router.currentRoute.value.query;
-    }
-    if (qs) {
-      const query = ref<string>('');
-      query.value += `?state=${qs.state}`;
-      query.value += `&code=${qs.code}`;
-      async function login(url: string) {
-        try {
-          const result = await oauthLoginCallback(url);
-          const { token } = result;
+    const query = ref<string>('');
+    query.value += `?state=${router.currentRoute.value.query.state}`;
+    query.value += `&code=${router.currentRoute.value.query.code}`;
 
-          const accessStore = useAccessStore();
-          const authStore = useAuthStore();
-          // save token
-          accessStore.setAccessToken(token);
-          await authStore.fetchUserInfo();
-        } catch {
-          message.error($t('sys.oauth.createAccount'), 5);
-          router.replace('/auth/login');
-        }
+    async function login(url: string) {
+      try {
+        const result = await oauthLoginCallback(url);
+        const { token } = result;
+
+        const accessStore = useAccessStore();
+        const authStore = useAuthStore();
+        // save token
+        accessStore.setAccessToken(token);
+        await authStore.fetchUserInfo();
+      } catch {
+        message.error($t('sys.oauth.createAccount'), 5);
+        router.replace('/auth/login');
       }
-
-      login(query.value);
-      return {};
     }
+
+    login(query.value);
+    return {};
   },
 });
 </script>
